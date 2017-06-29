75d47f952f4182c3916b36c7aaed5f7dbd820387
dean <99092651@qq.com>
2017-06-29 11:04:46 +0800

    加入数据库设置

---
+++ PCBQuotes/Helpers/AppConfigHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace PCBQuotes.Helpers
+{
+    public class AppConfigHelper
+    {
+        /// <summary>
+        /// 依据连接串名字connectionName返回数据连接字符串
+        /// </summary>
+        /// <param name="connectionName"></param>
+        /// <returns>连接字符串</returns>
+        public static string GetConnectionString(string connectionName)
+        {
+            if (ConfigurationManager.ConnectionStrings[connectionName] == null)
+            {
+                return string.Empty;
+            }
+            return ConfigurationManager.ConnectionStrings[connectionName].ConnectionString;
+        }
+
+        /// <summary>
+        /// 更新连接字符串
+        /// </summary>
+        /// <param name="connName">连接名称</param>
+        /// <param name="connString">连接字符串</param>
+        /// <param name="connProviderName">数据提供程序</param>
+        public static void UpdateConnectionString(string connName,string connString,string connProviderName)
+        {
+            //打开打开可执行的配置文件*.exe.config
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            
+            //如果有，先删除
+            if (ConfigurationManager.ConnectionStrings[connName] != null)
+            {
+                config.ConnectionStrings.ConnectionStrings.Remove(connName); 
+            }
+            //新建连接字符串
+            ConnectionStringSettings mySetting = new ConnectionStringSettings(connName,connString,connProviderName);
+            //将新的连接字符串添加到配置文件中
+            config.ConnectionStrings.ConnectionStrings.Add(mySetting);
+            //保存更改
+            config.Save(ConfigurationSaveMode.Modified);
+            // 强制重新载入配置文件的ConnectionStrings配置节
+            ConfigurationManager.RefreshSection("ConnectionStrings");
+        }
+    }
+}
--- PCBQuotes/UI/MainForm.cs
+++ PCBQuotes/UI/MainForm.cs
@@ -16,7 +16,8 @@ namespace PCBQuotes.UI
         {
             InitializeComponent();
             InitForm();
-            GetDatabaseConnectionString();
+
+            
         }
 
         /// <summary>
@@ -56,7 +57,44 @@ namespace PCBQuotes.UI
             //数据库设置菜单事件
             this.menuDatabaseSetting.Click += (s, e) => {
                 //Helpers.MDIFormHelper.OpenUniqueMDIChildWindow<UI.DatabaseSettingForm>(this);
+                SetDatabaseConnectionString();
             };
+        }
+
+        private void SetDatabaseConnectionString()
+        {
+            
+
+            DataConnectionDialog dialog = new DataConnectionDialog();
+            dialog.DataSources.Clear();
+
+            //添加数据源列表，可以向窗口中添加所需要的数据源类型 必须至少有一项
+            dialog.DataSources.Add(DataSource.AccessDataSource);    //Access
+            dialog.DataSources.Add(DataSource.SqlDataSource);       //Sql Server
+            dialog.DataSources.Add(DataSource.OracleDataSource);    //Oracle
+            dialog.DataSources.Add(DataSource.OdbcDataSource);      //Odbc
+            dialog.DataSources.Add(DataSource.SqlFileDataSource);   //Sql Server File
+
+            //设置默认数据提供程序
+            dialog.SelectedDataSource = DataSource.SqlDataSource;
+            dialog.SelectedDataProvider = DataProvider.SqlDataProvider;
+            dialog.Text = "数据库连接设置";
+
+            
+            var connStr = Helpers.AppConfigHelper.GetConnectionString("DefaultConnStr");
+            if (!string.IsNullOrWhiteSpace(connStr))
+            {
+                dialog.ConnectionString = connStr;
+            }
+
+            //dialog.Title = "Cosmic_Spy";
+            //dialog.ConnectionString = "Data Source=****;Initial Catalog=****;Integrated Security=True"; //也可以设置默认连接字符串
+            //只能够通过DataConnectionDialog类的静态方法Show出对话框，不能使用dialog.Show()或dialog.ShowDialog()来呈现对话框
+            if (DataConnectionDialog.Show(dialog) == DialogResult.OK)
+            {
+                Helpers.AppConfigHelper.UpdateConnectionString("DefaultConnStr", dialog.ConnectionString, dialog.SelectedDataProvider.Name);
+                //result = dialog.ConnectionString;
+            }
         }
 
         private string GetDatabaseConnectionString()
